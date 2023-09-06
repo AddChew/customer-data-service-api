034f5ea69d08b2658a7bdce5bf19fd20e387e0d5
AddChew <addison_chew@outlook.com>
2023-09-07 01:04:54 +0800

    add docstring for deployment

--- app/deployment/__init__.py
+++ app/deployment/__init__.py
@@ -1,12 +1,18 @@
 import os
 
 from ray import serve
+from fastapi import FastAPI
 from starlette.requests import Request
-from ray.serve._private.http_util import BufferedASGISender
+from ray.serve._private.http_util import BufferedASGISender, RawASGIResponse
 
 
-def setup_app():
-    from fastapi import FastAPI
+def setup_app() -> FastAPI:
+    """
+    Setup FastAPI application with GraphQL router attached.
+
+    Returns:
+        FastAPI: FastAPI application with GraphQL router attached.
+    """
     from strawberry import Schema
     from app.queries import Query
     from strawberry.fastapi import GraphQLRouter
@@ -23,11 +29,22 @@ def setup_app():
 
 @serve.deployment(name = 'customer_data_service', num_replicas = int(os.getenv("NUM_REPLICAS", 1)))
 class Deployment:
-    
+    """
+    Customer Data Service API Deployment.
+    """
     def __init__(self):
         self.app = setup_app()
 
-    async def __call__(self, request: Request):
+    async def __call__(self, request: Request) -> RawASGIResponse:
+        """
+        Method to handle incoming requests.
+
+        Args:
+            request (Request): Incoming request.
+
+        Returns:
+            RawASGIResponse: FastAPI application response.
+        """
         sender = BufferedASGISender()
         await self.app(request.scope, receive = request.receive, send = sender)
         return sender.build_asgi_response()
