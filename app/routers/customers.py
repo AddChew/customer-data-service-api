49cf1804cf3b47ba01652e1a0a6735a60fe3594b
AddChew <addison_chew@outlook.com>
2023-09-11 23:10:06 +0800

    add routers

---
+++ app/routers/customers.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from app.schemas import Message
+from app.services import queries
+from app.schemas import Customer
+
+
+router = APIRouter(
+    prefix = "/customers",
+    tags = ["Customers"],
+    responses = {401: {"model": Message}, 404: {"model": Message}},
+)
+
+
+@router.get(
+    path = "", 
+    summary = "Retrieve Customer Details", 
+    description = "Retrieve customer details by cif."
+)
+async def read_customer(cif: str) -> Customer:
+    """
+    Retrieve customer details by cif.
+
+    Args:
+        cif (str): Cif of customer to retrieve details on.
+
+    Returns:
+        Customer: Customer object.
+    """
+    return await queries.retrieve_customer(cif)
