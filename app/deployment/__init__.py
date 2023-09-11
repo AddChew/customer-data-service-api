606c4e03e140101c4132a184c87c0e330d4d0fc5
AddChew <addison_chew@outlook.com>
2023-09-11 23:07:43 +0800

    update deployment

--- app/deployment/__init__.py
+++ app/deployment/__init__.py
@@ -2,49 +2,20 @@ import os
 
 from ray import serve
 from fastapi import FastAPI
-from starlette.requests import Request
-from ray.serve._private.http_util import BufferedASGISender, RawASGIResponse
+from app.routers import customers
 
 
-def setup_app() -> FastAPI:
-    """
-    Setup FastAPI application with GraphQL router attached.
-
-    Returns:
-        FastAPI: FastAPI application with GraphQL router attached.
-    """
-    from strawberry import Schema
-    from app.queries import Query
-    from strawberry.fastapi import GraphQLRouter
-
-    app = FastAPI(
-        title = 'Customer Data Service API',
-    )
-    schema = Schema(query = Query)
-    graphql_app = GraphQLRouter(schema)
-    app.include_router(graphql_app, prefix = '/graphql')
-
-    return app
+app = FastAPI(
+    title = "Customer Data Service REST API",
+    description = "Documentation for Customer Data Service REST API"
+)
+app.include_router(customers.router)
 
 
 @serve.deployment(name = 'customer_data_service', num_replicas = int(os.getenv("NUM_REPLICAS", 1)))
+@serve.ingress(app)
 class Deployment:
     """
-    Customer Data Service API Deployment.
+    Customer Data Service REST API Deployment.
     """
-    def __init__(self):
-        self.app = setup_app()
-
-    async def __call__(self, request: Request) -> RawASGIResponse:
-        """
-        Method to handle incoming requests.
-
-        Args:
-            request (Request): Incoming request.
-
-        Returns:
-            RawASGIResponse: FastAPI application response.
-        """
-        sender = BufferedASGISender()
-        await self.app(request.scope, receive = request.receive, send = sender)
-        return sender.build_asgi_response()
+    pass
