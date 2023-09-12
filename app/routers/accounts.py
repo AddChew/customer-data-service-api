3996bd15f3c25db4eb331c6fb0bdf5a81e0ac5cc
AddChew <addison_chew@outlook.com>
2023-09-12 22:31:58 +0800

    update routers

--- app/routers/accounts.py
+++ app/routers/accounts.py
@@ -1,8 +1,9 @@
 from typing import List
 from fastapi import APIRouter
+
 from app.services import queries
-from app.schemas.account import Params
 from app.schemas import Account, Message
+from fastapi import HTTPException, status
 
 
 router = APIRouter(
@@ -17,17 +18,26 @@ router = APIRouter(
     summary = "Retrieve Account(s) Details", 
     description = "Retrieve account(s) details by cif and/or accNum."
 )
-async def read_accounts(params: Params) -> List[Account]:
+async def read_accounts(cif: str = None, accNum: str = None) -> List[Account]:
     """
     Retrieve account(s) details by cif and/or accNum.
 
     Args:
-        params (Params): Query params consisting of cif and/or accNum.
+        cif (str, optional): Cif of customer to retrieve accounts(s) for. Defaults to None.
+        accNum (str, optional): Account number of account to retrieve. Defaults to None.
+
+    Raises:
+        HTTPException: Raised when both cif and accNum are missing from the query parameters.
 
     Returns:
         List[Account]: List of Account objects.
     """
+    if cif is None and accNum is None:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Missing cif and/or accNum in query parameters."
+        )
     return await queries.retrieve_accounts(
-        cif = params.cif,
-        accNum = params.accNum,
+        cif = cif,
+        accNum = accNum,
     )
