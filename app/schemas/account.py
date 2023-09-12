0f8eae72bd85a4b3b5a3bb03487b95fb3c0a0a0e
AddChew <addison_chew@outlook.com>
2023-09-12 22:32:17 +0800

    remove params

--- app/schemas/account.py
+++ app/schemas/account.py
@@ -1,7 +1,5 @@
 from datetime import date
-from typing import Optional
-from pydantic.dataclasses import dataclass
-from pydantic import BaseModel, root_validator
+from pydantic import BaseModel
 
 
 class Account(BaseModel):
@@ -16,15 +14,3 @@ class Account(BaseModel):
     currency: str
     createDate: date
     accStatus: str
-
-
-@dataclass
-class Params:
-    cif: Optional[str] = None
-    accNum: Optional[str] = None
-
-    @root_validator
-    def check_cif_or_accNum(cls, v):
-        if not any(v.values()):
-            raise ValueError("Missing cif and/or accNum in query params")
-        return v
